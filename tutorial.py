2d514d6644e427af3053287999b84dc72eb33a3f
Noah Momblanco <nmombo@umich.edu>
2017-03-28 21:56:00 -0400

    add tutorial file

---
+++ tutorial.py
@@ -0,0 +1,5 @@
+# tutorial from http://docs.python-guide.org/en/latest/scenarios/scrape/
+
+# lxml is an extensice library written for parsing XML and HTML documents.
+from lxml import html
+# Requests is a module for http requests (I think)
