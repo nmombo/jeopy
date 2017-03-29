fc82843c700b02d4f54a8fea2773e06107614634
Noah Momblanco <nmombo@umich.edu>
2017-03-28 22:45:21 -0400

    add first attempt

---
+++ jeopy_single.py
@@ -0,0 +1,12 @@
+# jeopy_single.py
+# First attempt at scraping data from J! Archive. The plan is to scrape data
+# for just a single file before attempting to present it in a meaningful way
+# or iterating through multiple shows.
+
+# Author: Noah Momblanco
+#         http://github.com/nmombo/jeopy
+# Date: 3/28/2017
+
+from xlml import html
+import requests
+
